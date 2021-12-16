23ca28fa65321fe2e990f43e46675cba9a9a3dd8
Sonawane <ravi.sonawane@atos.net>
2021-12-16 23:33:55 +0530

    Added collection items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Route, Routes } from "react-router-dom";
-
+import ShopPage from "./pages/shop/shop.component";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
